aa2db6ba586cd3b7f7bb4720c8b877bf8e1095d6
Jake Griffin <jake.griffin@gmail.com>
2026-07-11 20:36:33 -0700

    Fix build/deployment configuration

--- infra/dynamodb.ts
+++ infra/dynamodb.ts
@@ -1,36 +1,31 @@
 // Optional CDK stack for provisioning the RefereeCallApp DynamoDB table.
-// Can be deployed with: npx cdk deploy
+// Can be deployed with: npx cdk deploy --app "npx ts-node --compiler-options '{\"module\":\"commonjs\"}' infra/dynamodb.ts"
 // Alternatively, create the table manually in the AWS Console.
 
-import { Stack, StackProps, App, RemovalPolicy } from 'aws-cdk-lib';
-import {
-  Table,
-  AttributeType,
-  BillingMode,
-  ProjectionType,
-} from 'aws-cdk-lib/aws-dynamodb';
+/* eslint-disable @typescript-eslint/no-require-imports */
+const cdk = require('aws-cdk-lib');
+const dynamodb = require('aws-cdk-lib/aws-dynamodb');
 
-export class RefereeCallAppStack extends Stack {
-  constructor(scope: App, id: string, props?: StackProps) {
+class RefereeCallAppStack extends cdk.Stack {
+  constructor(scope: any, id: string, props?: any) {
     super(scope, id, props);
 
-    const table = new Table(this, 'RefereeCallAppTable', {
+    const table = new dynamodb.Table(this, 'RefereeCallAppTable', {
       tableName: 'RefereeCallApp',
-      partitionKey: { name: 'PK', type: AttributeType.STRING },
-      sortKey: { name: 'SK', type: AttributeType.STRING },
-      billingMode: BillingMode.PAY_PER_REQUEST,
-      removalPolicy: RemovalPolicy.RETAIN,
+      partitionKey: { name: 'PK', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'SK', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
     });
 
     table.addGlobalSecondaryIndex({
       indexName: 'GSI1',
-      partitionKey: { name: 'GSI1PK', type: AttributeType.STRING },
-      sortKey: { name: 'GSI1SK', type: AttributeType.STRING },
-      projectionType: ProjectionType.ALL,
+      partitionKey: { name: 'GSI1PK', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'GSI1SK', type: dynamodb.AttributeType.STRING },
+      projectionType: dynamodb.ProjectionType.ALL,
     });
   }
 }
 
-// Instantiate the stack when run directly
-const app = new App();
+const app = new cdk.App();
 new RefereeCallAppStack(app, 'RefereeCallAppStack');
